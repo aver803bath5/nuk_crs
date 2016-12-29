6dc03c1cdd47fc00a48ea75dd5336a3d56192971
NoobTW <noob@noob.tw>
2016-12-29 10:00:10 +0800

    add common pugs

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	db: {
+		host: 'localhost',
+		name: 'crs',
+		user: 'crs',
+		pass: 'crs'
+	},
+	secret: {
+		cookie: 'qru9aoxaokt3vi8o2927tr99h3ytnfhpqrshifwnkd6o2j03ry449y80iboo0nctjuh1ho657pdr8u2zuirxbudj6z8vmx46yy0nrv14im70kcbiaoq2dg7zntbpzv27ac3gd37jv4cyy0ph9iho2k11xj5jq4w9v80upnbo7debpbjeuaiissblry9g4npnta6ij00e6ai973fjj32ttbuse3z5wsyfdvvp8hs6svhpsnja9xgghhsy36bx549b',
+		session: 'qru9aoxaokt3vi8o2927tr99h3ytnfhpqrshifwnkd6o2j03ry449y80iboo0nctjuh1ho657pdr8u2zuirxbudj6z8vmx46yy0nrv14im70kcbiaoq2dg7zntbpzv27ac3gd37jv4cyy0ph9iho2k11xj5jq4w9v80upnbo7debpbjeuaiissblry9g4npnta6ij00e6ai973fjj32ttbuse3z5wsyfdvvp8hs6svhpsnja9xgghhsy36bx549b'
+	}
+}
+
