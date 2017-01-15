e5ecdad1d46c1b90c9ec1ffc286c11169f1b8565
aver803bath5 <aver803bath261@gmail.com>
2017-01-16 04:13:18 +0800

    add login condition

--- public/js/all.js
+++ public/js/all.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	$('#login').on('submit', function(){
-		$('input[name=pass]').val(md5($('input[name=pass]').val()));
+		$('input[name=password]').val(md5($('input[name=password]').val()));
 	});
 });
