6d2f3ad4a90d9561c81b44a19247d096c87e19d8
NoobTW <noob@noob.tw>
2017-03-01 22:39:38 +0800

    fix not to ne

--- server.js
+++ server.js
@@ -827,7 +827,7 @@ app
 .get('/admin/userlist', (req, res) => {
 	const sess = req.session;
 	if(sess.user && sess.user.is_root){
-		db.collection('user').find({username: {$not: '通識教育中心'}}).toArray((resp, docs) => {
+		db.collection('user').find({username: {$ne: '通識教育中心'}}).toArray((resp, docs) => {
 			if(docs.length){
 				res.render('users', {
 					users: docs,
